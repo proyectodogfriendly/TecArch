ed305aa040e8598fbd405a406b6f4c3d4555c59a
RebecaLS <rebeca.lopez.sae@gmail.com>
2019-08-28 13:14:57 +0200

    dep4

--- Server/app.js
+++ Server/app.js
@@ -51,9 +51,9 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "hbs");
+
 app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+
 
 // Configuración de sesión
 app.use(
